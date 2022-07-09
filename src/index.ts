016868ed0bd8177da87400159f71b227bb38ee1b
jpmasters <jon.masters@visyonr.io>
2022-07-09 14:49:26 +0100

    Removed unnecessary comment from index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import { SudokuSolver } from './SudokuSolver';
 import { SudokuSolverStrategy, SudokuSolverOptions } from './SudokuSolverOptions';
 
-// TODO: Export options and solver enum and make sure it works in a Typescript project
 export = { SudokuSolver, SudokuSolverStrategy, SudokuSolverOptions };
