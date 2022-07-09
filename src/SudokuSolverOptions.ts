386600daf4765ca210d82da6c42802971479bd93
jpmasters <jon.masters@visyonr.io>
2022-07-09 14:48:21 +0100

    Updated index to export options related types

--- src/SudokuSolverOptions.ts
+++ src/SudokuSolverOptions.ts
@@ -1,3 +1,7 @@
+/**
+ * An enumeration of available solving strategies. Note that backtracking
+ * is handled separately in the options.
+ */
 export enum SudokuSolverStrategy {
   CollapsedValues,
   HiddenPairs,
@@ -6,17 +10,38 @@ export enum SudokuSolverStrategy {
   SingleValues,
 }
 
-export interface SudokuSolverOptions {
+/**
+ * Defines the available options for solving the puzzle.
+ */
+export class SudokuSolverOptions {
+  /**
+   * An array of SudokuSolverStrategy values representing the solvers to include.
+   * If an array isn't provided, it will include all solvers by default. To run
+   * the backtracking algorithm on its own, specify an empty array i.e. [].
+   */
   includeStrategies?: SudokuSolverStrategy[];
+
+  /**
+   * Defines whether or not to complete puzzles using backtracking. If the value is
+   * not specified or set to true, backtracking will be used to fill in any unknown
+   * cells. If it is explicitly set to false, it will not run backtracking. Note that
+   * this might result in outputting incomlpete puzzles but it's useful when writing new
+   * solver classes.
+   */
   includeBacktracking?: boolean;
 }
 
+/**
+ * Fills in any missing default values not provided by the calling application.
+ * @param providedOptions Options provided by the calling application.
+ * @returns Options with defaults filled in where unspecified.
+ */
 export function deriveOptions(providedOptions: SudokuSolverOptions): SudokuSolverOptions {
-    return {
-      includeBacktracking: true,
-      includeStrategies: Object.values(SudokuSolverStrategy).filter(
-        (v) => typeof v !== 'string',
-      ) as SudokuSolverStrategy[],
-      ...providedOptions,
-    };
+  return {
+    includeBacktracking: true,
+    includeStrategies: Object.values(SudokuSolverStrategy).filter(
+      (v) => typeof v !== 'string',
+    ) as SudokuSolverStrategy[],
+    ...providedOptions,
+  };
 }
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 import { SudokuSolver } from './SudokuSolver';
+import { SudokuSolverStrategy, SudokuSolverOptions } from './SudokuSolverOptions';
 
-export = SudokuSolver;
+// TODO: Export options and solver enum and make sure it works in a Typescript project
+export = { SudokuSolver, SudokuSolverStrategy, SudokuSolverOptions };
